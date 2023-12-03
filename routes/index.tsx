7274951f8edc6475ebca3afd08e47d26e8a4251c
omsritn <reallydeepwater@proton.me>
2023-12-04 07:12:45 +0900

    .

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       <Head>
         <title>Flow</title>
         <link rel="stylesheet" href="/style.css" />
-        <link rel="stylesheet" href="/flow.css" />
       </Head>
 
       <canvas id="flow" height="720px" width="1280px" style="height: 360px; width: 640px;"></canvas>
